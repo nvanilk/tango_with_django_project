ef0ff5a81907db91a55225743e98909838ad8e77
N.V. Anil Kumar <nvanilk@gmail.com>
2017-04-24 21:33:55 +0530

    Initial commit

---
+++ rango/urls.py
@@ -0,0 +1,11 @@
+#Created by Anil.
+
+from django.conf.urls import url
+from rango import views
+
+urlpatterns=[
+
+    url(r'^$', views.index, name='index'), 
+    url(r'^about/', views.about, name='about'),
+
+]
